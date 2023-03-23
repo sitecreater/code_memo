105fe026a19c7168957ec4f5a91f063c87cb1e46
sitecreater <pk8008@naver.com>
2023-03-23 19:45:18 +0900

    .env update

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,11 +4,11 @@ let conn: any;
 
 if (!conn) {
   conn = new Pool({
-    user: "root",
-    password: "root",
-    host: "localhost",
-    port: 5432,
-    database: "tasksdb",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 }
 
